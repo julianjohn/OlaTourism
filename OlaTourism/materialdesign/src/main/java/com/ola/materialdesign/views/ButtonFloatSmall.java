a7e4dc48924084fae1f6436cf504f213fc59dd0e
Prakash Nandi <PrakashNandi@Prakashs-MacBook-Pro.local>
2015-09-26 18:30:51 +0530

    added material design module

---
+++ OlaTourism/materialdesign/src/main/java/com/ola/materialdesign/views/ButtonFloatSmall.java
@@ -0,0 +1,34 @@
+package com.ola.materialdesign.views;
+
+import com.ola.materialdesign.R;
+import com.ola.materialdesign.utils.Utils;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class ButtonFloatSmall extends ButtonFloat {
+	
+	public ButtonFloatSmall(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		sizeRadius = 20;
+		sizeIcon = 20;
+		setDefaultProperties();
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(Utils.dpToPx(sizeIcon, getResources()),Utils.dpToPx(sizeIcon, getResources()));
+		params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+		icon.setLayoutParams(params);
+	}
+	
+	protected void setDefaultProperties(){
+		rippleSpeed = Utils.dpToPx(2, getResources());
+		rippleSize = 10;		
+		// Min size
+		setMinimumHeight(Utils.dpToPx(sizeRadius*2, getResources()));
+		setMinimumWidth(Utils.dpToPx(sizeRadius*2, getResources()));
+		// Background shape
+		setBackgroundResource(R.drawable.background_button_float2);
+//		setBackgroundColor(Color.parseColor("#FFFFFF"));
+	}
+
+}
